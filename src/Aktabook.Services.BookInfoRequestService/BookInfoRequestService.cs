8218eca75e63baba9a0540643f378878d07fa3bb
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-06-29 23:37:03 +0200

    chore: Add missing header

--- src/Aktabook.Services.BookInfoRequestService/BookInfoRequestService.cs
+++ src/Aktabook.Services.BookInfoRequestService/BookInfoRequestService.cs
@@ -1,3 +1,9 @@
+// Copyright (c) 2022 Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
 using Aktabook.Application.Services;
 using Aktabook.Data;
 using Aktabook.Domain.Models;
