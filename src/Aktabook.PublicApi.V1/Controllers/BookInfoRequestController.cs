0a91195135f56e660acf745c39d0f5f8e61bb424
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-07-02 08:44:57 +0200

    feat: Add BookInfoRequestController

---
+++ src/Aktabook.PublicApi.V1/Controllers/BookInfoRequestController.cs
@@ -0,0 +1,58 @@
+// Copyright (c) 2022 Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+using Aktabook.Application.Commands;
+using Aktabook.PublicApi.V1.Dto;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aktabook.PublicApi.V1.Controllers;
+
+[ApiController]
+[ApiConventionType(typeof(DefaultApiConventions))]
+[Route("api/[controller]")]
+public class BookInfoRequestController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BookInfoRequestController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+    public ActionResult<IEnumerable<object>> Get()
+    {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status501NotImplemented)]
+    public ActionResult<object> Get(Guid id)
+    {
+        return StatusCode(StatusCodes.Status501NotImplemented);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<CreateBookInfoRequestResponse>> Post(
+        [FromBody]
+        CreateBookInfoRequestRequest createBookInfoRequestRequest)
+    {
+        PlaceBookInfoRequest placeBookInfoRequest =
+            new(createBookInfoRequestRequest.Isbn);
+
+        Guid bookInfoRequestId = await _mediator.Send(placeBookInfoRequest);
+
+        CreateBookInfoRequestResponse response = new()
+        {
+            BookInfoRequestId = bookInfoRequestId
+        };
+
+        return CreatedAtAction(nameof(Get), response.BookInfoRequestId,
+            response);
+    }
+}
