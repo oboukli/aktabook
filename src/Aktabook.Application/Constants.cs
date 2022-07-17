e6328ca3a0f4b750713d8391e84c76b998eb1e75
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-07-17 17:24:48 +0200

    chore: Fix code smells

---
+++ src/Aktabook.Application/Constants.cs
@@ -0,0 +1,15 @@
+// Copyright (c) 2022 Omar Boukli-Hacene. All rights reserved.
+// Distributed under an MIT-style license that can be
+// found in the LICENSE file.
+
+// SPDX-License-Identifier: MIT
+
+namespace Aktabook.Application;
+
+public static class Constants
+{
+#pragma warning disable S1075
+    public const string AppLicenseUrl =
+        "https://github.com/oboukli/aktabook/blob/main/LICENSE";
+#pragma warning restore
+}
