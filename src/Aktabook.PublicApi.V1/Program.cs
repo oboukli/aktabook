c787ed1381f7088b8fb915c0e22fe5998991cbc1
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2022-07-28 02:14:27 +0200

    feat: Add structured logger

--- src/Aktabook.PublicApi.V1/Program.cs
+++ src/Aktabook.PublicApi.V1/Program.cs
@@ -13,49 +13,94 @@ using FluentValidation.AspNetCore;
 using MicroElements.Swashbuckle.FluentValidation.AspNetCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
+using Serilog;
+using Serilog.Exceptions;
+using Serilog.Exceptions.Core;
+using Serilog.Exceptions.EntityFrameworkCore.Destructurers;
+using Serilog.Formatting.Compact;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+const string bootstrapLogFileName = "aktabook-api-bootstrap.log";
 
-builder.Services.AddControllers()
-    .AddFluentValidation(x =>
-    {
-        x.DisableDataAnnotationsValidation = true;
-        x.RegisterValidatorsFromAssemblyContaining<
-            CreateBookInfoRequestRequestValidator>();
-    });
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .Enrich.WithExceptionDetails()
+    .WriteTo.Console()
+    .WriteTo.File(
+        new CompactJsonFormatter(),
+        bootstrapLogFileName,
+        rollingInterval: RollingInterval.Infinite,
+        rollOnFileSizeLimit: false,
+        fileSizeLimitBytes: 32 * 1024 * 1024
+    )
+    .CreateBootstrapLogger();
 
-builder.Services.AddApplicationServices(options =>
-    options.UseSqlServer(builder.Configuration
-        .GetSqlConnectionStringBuilderFrom(DbContextConstants
-            .RequesterServiceDbContextSqlServerSection).ConnectionString));
+Log.Information("Starting host");
 
-builder.Services.AddHealthChecks();
-builder.Services.AddSwaggerGen(options =>
+try
 {
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Aktabook",
-        Description = "A book data aggregator API",
-        License = new OpenApiLicense
+    WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog((context, services, loggerConfiguration) =>
+        loggerConfiguration.ReadFrom.Configuration(context.Configuration)
+            .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder()
+                .WithDefaultDestructurers()
+                .WithDestructurers(
+                    new[] { new DbUpdateExceptionDestructurer() })
+            )
+    );
+
+    builder.Services.AddControllers()
+        .AddFluentValidation(x =>
         {
-            Name = "MIT",
-            Url = new Uri(Constants.AppLicenseUrl)
-        }
+            x.DisableDataAnnotationsValidation = true;
+            x.RegisterValidatorsFromAssemblyContaining<
+                CreateBookInfoRequestRequestValidator>();
+        });
+
+    builder.Services.AddApplicationServices(options =>
+        options.UseSqlServer(builder.Configuration
+            .GetSqlConnectionStringBuilderFrom(DbContextConstants
+                .RequesterServiceDbContextSqlServerSection).ConnectionString));
+
+    builder.Services.AddHealthChecks();
+    builder.Services.AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Aktabook",
+            Description = "A book data aggregator API",
+            License = new OpenApiLicense
+            {
+                Name = "MIT",
+                Url = new Uri(Constants.AppLicenseUrl)
+            }
+        });
     });
-});
-builder.Services.AddFluentValidationRulesToSwagger();
+    builder.Services.AddFluentValidationRulesToSwagger();
 
-WebApplication app = builder.Build();
+    WebApplication app = builder.Build();
 
-app.UseRouting();
-app.UseEndpoints(configure =>
+    app.UseRouting();
+    app.UseEndpoints(configure =>
+    {
+        configure.MapControllers();
+    });
+    app.UseHealthChecks("/healthz");
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+    app.Run();
+
+    return 0;
+}
+catch (Exception ex)
 {
-    configure.MapControllers();
-});
-app.UseHealthChecks("/healthz");
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.Run();
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
