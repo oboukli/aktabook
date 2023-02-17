66b115ac7672e950ccb33712901d90e82983b0bf
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-02-18 00:27:42 +0100

    test: Fix API integration test configuration

--- test/Aktabook.PublicApi.V1.IntegrationTest/TestingWebApplicationFixture.cs
+++ test/Aktabook.PublicApi.V1.IntegrationTest/TestingWebApplicationFixture.cs
@@ -4,18 +4,45 @@
 
 // SPDX-License-Identifier: MIT
 
+using System;
+using System.IO;
 using System.Net.Http;
+using Aktabook.Data;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Aktabook.PublicApi.V1.IntegrationTest;
 
 public class TestingWebApplicationFixture<TStartup> where TStartup : class
 {
     private readonly WebApplicationFactory<TStartup> _application;
+    private readonly IServiceScope _serviceScope;
+
+    public RequesterServiceDbContext RequesterServiceDbContext { get; }
 
     public TestingWebApplicationFixture()
     {
-        _application = new WebApplicationFactory<TStartup>();
+        _application = new WebApplicationFactory<TStartup>()
+            .WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureAppConfiguration((context, configBuilder) =>
+                {
+                    configBuilder.Sources.Clear();
+                    string? environmentName = Environment.GetEnvironmentVariable("AKTABOOK_INTEGRATION_TEST_ENVIRONMENT");
+
+                    configBuilder.SetBasePath(Directory.GetCurrentDirectory());
+                    configBuilder.AddJsonFile("appsettings.json", true);
+                    if (!string.IsNullOrEmpty(environmentName))
+                    {
+                        configBuilder.AddJsonFile($"appsettings.{environmentName}.json", true);
+                    }
+                    configBuilder.AddEnvironmentVariables();
+                });
+            });
+
+        _serviceScope = _application.Server.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+        RequesterServiceDbContext = _serviceScope.ServiceProvider.GetRequiredService<RequesterServiceDbContext>();
     }
 
     public HttpClient CreateClient()
