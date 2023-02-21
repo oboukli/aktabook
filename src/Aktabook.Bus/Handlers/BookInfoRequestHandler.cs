f5b88ef78b1770082a172b374a4d18f3ea7c62ea
Omar Boukli-Hacene <oboukli@users.noreply.github.com>
2023-02-21 22:29:22 +0100

    feat: Add basic book information persistence

--- src/Aktabook.Bus/Handlers/BookInfoRequestHandler.cs
+++ src/Aktabook.Bus/Handlers/BookInfoRequestHandler.cs
@@ -10,6 +10,7 @@ using Aktabook.Application.Messages.Events;
 using Aktabook.Application.Services;
 using Aktabook.Connectors.OpenLibrary;
 using Aktabook.Connectors.OpenLibrary.Models;
+using Aktabook.Data;
 using Aktabook.Domain.Models;
 using NServiceBus.Logging;
 
@@ -22,14 +23,17 @@ public class BookInfoRequestHandler : IHandleMessages<ProcessBookInfoRequest>
 
     private readonly IBookInfoRequestService _bookInfoRequestService;
     private readonly IOpenLibraryClient _openLibraryClient;
+    private readonly RequesterServiceDbContext _requesterServiceDbContext;
 
     public BookInfoRequestHandler(
         IBookInfoRequestService bookInfoRequestService,
         IOpenLibraryClient openLibraryClient,
+        RequesterServiceDbContext requesterServiceDbContext,
         ActivitySource activitySource)
     {
         _bookInfoRequestService = bookInfoRequestService;
         _openLibraryClient = openLibraryClient;
+        _requesterServiceDbContext = requesterServiceDbContext;
         _activitySource = activitySource;
     }
 
@@ -40,34 +44,67 @@ public class BookInfoRequestHandler : IHandleMessages<ProcessBookInfoRequest>
         activity?.AddEvent(new ActivityEvent(nameof(ChangeRequestStatus)));
         activity?.SetTag(Constants.TelemetryTags.FunctionTagKey, Constants.TelemetryTags.FunctionTagValue);
 
-        Log.Info(
-            $@"{nameof(ProcessBookInfoRequest)} received ""{message.BookInfoRequestId}""");
+        Log.Info($@"{nameof(ProcessBookInfoRequest)} received ""{message.BookInfoRequestId}""");
 
-        await ChangeRequestStatus(message.BookInfoRequestId, BookInfoRequestStatus.InProgress).ConfigureAwait(false);
+        await ChangeRequestStatus(message.BookInfoRequestId, BookInfoRequestStatus.InProgress)
+            .ConfigureAwait(false);
 
-        await context.Publish(new BookInfoRequestStatusChanged(message.BookInfoRequestId, BookInfoRequestStatus.InProgress)).ConfigureAwait(false);
+        await context
+            .Publish(new BookInfoRequestStatusChanged(message.BookInfoRequestId,
+                BookInfoRequestStatus.InProgress)).ConfigureAwait(false);
 
         activity?.AddEvent(new ActivityEvent(nameof(IOpenLibraryClient.GetBookByIsbnAsync)));
 
-        Work? work = await _openLibraryClient.GetBookByIsbnAsync(message.Isbn, CancellationToken.None).ConfigureAwait(false);
+        string requestStatus;
+        Work? work = await _openLibraryClient.GetBookByIsbnAsync(message.Isbn, CancellationToken.None)
+            .ConfigureAwait(false);
+        if (work is { })
+        {
+            await CreateBookFromWork(work, CancellationToken.None).ConfigureAwait(false);
+            requestStatus = BookInfoRequestStatus.Fulfilled;
+        }
+        else
+        {
+            requestStatus = BookInfoRequestStatus.Failed;
+        }
 
         activity?.AddEvent(new ActivityEvent(nameof(ChangeRequestStatus)));
 
-        await ChangeRequestStatus(message.BookInfoRequestId, BookInfoRequestStatus.Fulfilled)
+        await ChangeRequestStatus(message.BookInfoRequestId, requestStatus)
             .ConfigureAwait(false);
 
         await context.Publish(new BookInfoRequestProcessed(message.BookInfoRequestId)).ConfigureAwait(false);
 
-        await context.Publish(new BookInfoRequestStatusChanged(message.BookInfoRequestId, BookInfoRequestStatus.Fulfilled)).ConfigureAwait(false);
+        await context.Publish(new BookInfoRequestStatusChanged(message.BookInfoRequestId, requestStatus))
+            .ConfigureAwait(false);
     }
 
     private async Task ChangeRequestStatus(Guid bookInfoRequestId, string status)
     {
-        bool success = await _bookInfoRequestService.ChangeRequestStatus(bookInfoRequestId, status, CancellationToken.None).ConfigureAwait(false);
+        bool success = await _bookInfoRequestService
+            .ChangeRequestStatus(bookInfoRequestId, status, CancellationToken.None).ConfigureAwait(false);
 
         if (success is false)
         {
             Log.Error($@"{nameof(ProcessBookInfoRequest)} change status failed ""{bookInfoRequestId}""");
         }
+    }
+
+    private async Task CreateBookFromWork(Work work, CancellationToken cancellationToken)
+    {
+        Book book = WorkToBook(work);
+        await _requesterServiceDbContext.Books.AddAsync(book, cancellationToken)
+            .ConfigureAwait(false);
+        await _requesterServiceDbContext.SaveChangesAsync(cancellationToken).ConfigureAwait(false);
+    }
+
+    private static Book WorkToBook(Work work)
+    {
+        string isbn10 = work.Isbn10.FirstOrDefault() ?? string.Empty;
+        return new Book
+        {
+            Isbn = isbn10,
+            Title = work.Title
+        };
     }
 }
